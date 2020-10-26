200ec0ac1920d12692c905b8a3e46d749cd99462
stevenjnovotny <steven.j.novotny@gmail.com>
2020-10-26 11:00:42 -0600

    updated mission altitude

--- backyard_flyer.py
+++ backyard_flyer.py
@@ -95,10 +95,10 @@ class BackyardFlyer(Drone):
         """
         1. Return waypoints to fly a box
         """
-        self.all_waypoints.append(np.array([0.0, 10.0, 10.0]))
-        self.all_waypoints.append(np.array([10.0, 10.0, 10.0]))
-        self.all_waypoints.append(np.array([10.0, 0.0, 10.0]))
-        self.all_waypoints.append(np.array([0.0, 0.0, 10.0]))  
+        self.all_waypoints.append(np.array([0.0, 10.0, 3.0]))
+        self.all_waypoints.append(np.array([10.0, 10.0, 3.0]))
+        self.all_waypoints.append(np.array([10.0, 0.0, 3.0]))
+        self.all_waypoints.append(np.array([0.0, 0.0, 3.0]))  
         #self.all_waypoints.reverse()      
 
 
